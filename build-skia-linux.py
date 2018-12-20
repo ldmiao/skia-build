06177beed6cbfa0037659327a2299d7752660eb4
ldmiao <ldmiao@gmail.com>
2018-12-20 23:47:01 +0800

    Update and rename build-skia.py to build-skia-linux.py

--- build-skia-linux.py
+++ build-skia-linux.py
@@ -32,6 +32,8 @@ run(['git', '-C', 'skia', 'checkout', args.branch])
 run(['git', '-C', 'skia', 'pull'])
 run(['python', 'skia/tools/git-sync-deps'])
 
+run(['tar', '-czf', 'skia-all-linux.tar.gz', 'skia', 'depot_tools'])
+
 # build
 shared_args = [
     'is_debug=false',
